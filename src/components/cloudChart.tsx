5829ff707d8fe84eca22c1458ce441087fd6b2cb
UbuntuRoot <757909414@qq.com>
2023-02-06 20:54:15 +0800

    注释

--- src/components/cloudChart.tsx
+++ src/components/cloudChart.tsx
@@ -53,6 +53,7 @@ export default function CloudChart() {
       window.addEventListener('resize', function() {
         myChart.resize();
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
 
